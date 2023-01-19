ba4d5e545b9d0e8192835763d694ef6c40e6510a
Orestes Febles <ofebles@gmail.com>
2023-01-19 00:41:59 +0000

    mantine initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import "@/styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+
+import { MantineProvider } from "@mantine/core";
+import { NextPage } from "next";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 export default function App({
   Component,
@@ -12,13 +24,23 @@ export default function App({
 }>) {
   // Create a new supabase browser client on every first render.
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
-
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        getLayout(
+        <Component {...pageProps} />)
+      </MantineProvider>
     </SessionContextProvider>
   );
 }
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import { Authentication } from "@/components/auth/Authentication";
+
+export default function Login() {
+  return (
+    <>
+      <Authentication />
+    </>
+  );
+}
